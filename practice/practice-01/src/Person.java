06a9e7db62dc80663114ef23fc4059f42b4a3898
Giorgi Beriashvili <giorgi.beriashvili@outlook.com>
2019-09-27 12:46:34 +0400

    Add basic class implementation

---
+++ practice/practice-01/src/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+    String firstName;
+    public String lastName;
+    private int age;
+
+    public void printData() {
+        System.out.println(String.format("%s %s", this.firstName, this.lastName));
+    }
+}
