19fdbb9ce8163401a10b9161e2fe5850fce4367a
Giorgi Beriashvili <giorgi.beriashvili@outlook.com>
2019-12-22 18:54:27 +0400

    Relocate midterm directory

---
+++ exam/midterm/code/src/dev/beriashvili/exam/Main.java
@@ -0,0 +1,14 @@
+package dev.beriashvili.exam;
+
+import dev.beriashvili.exam.exercises.DecimalToBinaryWriter;
+import dev.beriashvili.exam.exercises.Matrix;
+
+public class Main {
+    public static void main(String[] args) {
+        Matrix matrix = new Matrix();
+        matrix.main();
+
+        DecimalToBinaryWriter decimalToBinaryWriter = new DecimalToBinaryWriter();
+        decimalToBinaryWriter.main();
+    }
+}
---
+++ exam/midterm/code/src/dev/beriashvili/exam/exercises/DecimalToBinaryWriter.java
@@ -0,0 +1,75 @@
+package dev.beriashvili.exam.exercises;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class DecimalToBinaryWriter {
+    /*
+     * დაწერეთ პროგრამა, რომელიც კლავიატურიდან შეტანილ
+     * ათობით რიცხვს გადაიყვანს ორობითში და ჩაწერს
+     * number.txt ფაილში.
+     * */
+    private int decimal;
+    private String binary;
+
+    public void main() {
+        decimal = getInput();
+        binary = decimalToBinary();
+        writeToFile();
+    }
+
+    private int getInput() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a decimal: ");
+        decimal = scanner.nextInt();
+
+        return decimal;
+    }
+
+    private String decimalToBinary() {
+        StringBuilder binary = new StringBuilder();
+        Stack<Integer> digits = new Stack<>();
+
+        int origin = decimal;
+
+        if (decimal >= 0) {
+            binary.append("0 ");
+        } else {
+            binary.append("1 ");
+        }
+
+        decimal = Math.abs(decimal);
+
+        while (this.decimal != 0) {
+            int digit = decimal % 2;
+
+            digits.push(digit);
+
+            decimal /= 2;
+        }
+
+        while (!digits.empty()) {
+            binary.append(digits.pop());
+        }
+
+        this.decimal = origin;
+
+        return binary.toString();
+    }
+
+    private void writeToFile() {
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("data/number.txt", true));
+
+            bufferedWriter.write(String.format("Decimal %d to binary: %s\n", this.decimal, this.binary));
+
+            bufferedWriter.close();
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+    }
+}
